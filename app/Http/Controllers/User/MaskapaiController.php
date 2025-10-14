a762f81f0389eb3840214f83cadfc7bbbaed4cff
2ndmurphy <muhammad.jibrilmuqoddas@gmail.com>
2025-10-14 11:29:53 +0700

    fix undefined route

--- app/Http/Controllers/User/MaskapaiController.php
+++ app/Http/Controllers/User/MaskapaiController.php
@@ -9,6 +9,6 @@ class MaskapaiController extends Controller
 {
     public function index()
     {
-        return view('pages.maskapai.dashboard');
+        return view('pages.airline.dashboard');
     }
 }
